2c4e56078aa25e6e02e2297354343152c933332b
Elena Algaria <34727077+ElenaAlgaria@users.noreply.github.com>
2018-03-25 16:20:36 +0200

    Create MonsterCute.java

---
+++ MonsterCute.java
@@ -0,0 +1,16 @@
+package factory.design.pattern;
+
+/**
+ *
+ * @author Elena
+ */
+public class MonsterCute extends Monster{
+
+    public MonsterCute() {
+    
+    setName("Cute Monster");
+   
+ 
+    
+    }
+    }
