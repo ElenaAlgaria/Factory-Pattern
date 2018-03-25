6960950063ac9fb8da93c0a372ad47d2ccdaa40c
Elena Algaria <34727077+ElenaAlgaria@users.noreply.github.com>
2018-03-25 16:21:56 +0200

    Create MonsterBad.java

---
+++ MonsterBad.java
@@ -0,0 +1,15 @@
+package factory.design.pattern;
+
+/**
+ *
+ * @author Elena
+ */
+public class MonsterBad extends Monster{
+
+    public MonsterBad() {
+    
+    setName("Bad Monster");
+   
+    
+    }
+    }
